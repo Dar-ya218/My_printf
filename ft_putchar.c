fcf8d14a5d6f0cb8965b2f749bcc27d4a90b1bd3
dabochko <dabochko@student.42barcelona.com>
2024-03-07 15:11:38 +0100

    final

--- ft_putchar.c
+++ ft_putchar.c
@@ -11,9 +11,9 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-/*It takes a character 'c' as an argument and writes it to the standard output
-using the 'write()' function. 'write(1, &c, 1)' writes a single character
-(&c) to file descriptor 1 (which is the standard output).*/
+/*toma un carácter c como argumento y lo escribe en la salida estándar
+utilizando la función write(). write(1, &c, 1) escribe un solo carácter
+(&c) en el descriptor de archivo 1 (que es la salida estándar).*/
 int	ft_putchar(int c)
 {
 	if (write(1, &c, 1) < 0)
