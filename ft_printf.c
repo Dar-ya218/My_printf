94cd6c153d924c5e15c813f34b7771c777b63979
dabochko <dabochko@student.42barcelona.com>
2024-03-12 13:21:55 +0100

    make printf better

--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,22 @@
 /*   By: dabochko <dabochko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 15:10:23 by dabochko          #+#    #+#             */
-/*   Updated: 2024/03/08 14:13:41 by dabochko         ###   ########.fr       */
+/*   Updated: 2024/03/12 11:29:41 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-/* Toma una cadena de formato y un número variable de argumentos. 
-Utiliza va_start() para inicializar una lista de argumentos args. 
-Luego, recorre la cadena de formato carácter por carácter. Si 
-encuentra un carácter %, espera que el siguiente carácter sea un 
-especificador de formato (c, s o d en este caso) y llama a la 
-función correspondiente para escribir el siguiente argumento en 
-la salida estándar. Si el carácter no es un %, simplemente lo 
+//#include <stdio.h>
+//#include <stddef.h>
+
+/* Toma una cadena de formato y un número variable de argumentos.
+Utiliza	va_start(void) para inicializar una lista de argumentos args.
+Luego, recorre la cadena de formato carácter por carácter. Si
+encuentra un carácter %, espera que el siguiente carácter sea un
+especificador de formato (c, s o d en este caso) y llama a la
+función correspondiente para escribir el siguiente argumento en
+la salida estándar. Si el carácter no es un %, simplemente lo
 escribe en la salida estándar. Finalmente, utiliza
 va_end() para limpiar args.*/
 
@@ -43,7 +46,7 @@ static int	ft_selector(va_list args, char const place)
 			return (-1);
 		size += 2 + ft_putptr(va_arg(args, unsigned long));
 	}
-	else
+	else if (place == '%')
 		size += ft_putchar(place);
 	return (size);
 }
@@ -61,31 +64,40 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i])
 	{
-		if (format[i] == '%') //4. i == 2 | format[i] == ' ' //7. i == 3 | format[i] == '%'
-		{
-			//1. i == 0 | format[i] == '%'
-			size = ft_selector(args, format[i + 1]); //format[i + 1] == 'c' | i == 0
-			i++;
-			//2. i == 1 | format[i] == 'c'
-		}
+		if (format[i] == '%')
+			size = ft_selector(args, format[++i]);
 		else
-			size = ft_putchar(format[i]); //5. i == 2 | format[i] == ' '
+			size = ft_putchar(format[i]);
 		if (size == -1)
 			return (-1);
 		if (format[i] != '\0')
-			i++; //3. i == 2 //6. i == 3
+			i++;
 		size2 += size;
 	}
 	va_end(args);
 	return (size2);
 }
-
-int main(void) 
+/*
+int	main(void)
 {
-	//int i;
-    //i = ft_printf("hola %c %s %i %d %u %x %X",\
-	//'c', NULL, 547, -13, -1, 255, 255);
+	int	i;
+	int	i;
+	int	j;
+
+	i = ft_printf("hola %c %s %i %d %u %x %X %p%\0 57",\
+	'c', NULL, 547, -13, -1, 255, 255, &i);
 	//printf("\n%d\n", i);
 	ft_printf("%"); // '%' '\0' == 2
-    return (0);
+	return (0);
+}*/
+/*
+int	main(void)
+{
+	i = printf ("%%%c%s%p%u%d%i%x%X\n",\
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	j = ft_printf ("%%%c%s%p%u%d%i%x%X\n",\
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	printf("ORIGINAL %d\nMIO %d", i, j);
+	return ((0));
 }
+*/
